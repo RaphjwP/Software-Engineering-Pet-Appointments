0f404546605fe73cc98eee00f38e8502f44d1313
Jaewon Park <raph.ej.p@outlook.com>
2021-04-18 01:20:35 -0400

    TimePicker Added Recyclerview Fixed

--- app/src/main/java/com/example/meetmypetbuddy/models/Review.kt
+++ app/src/main/java/com/example/meetmypetbuddy/models/Review.kt
@@ -7,4 +7,5 @@ import java.time.format.DateTimeFormatter
 data class Review(var written_by:String, var review_description: String, var clinic_name:String, var doctor_name:String, var date: String, var rating: Float, var feedback_given:String){
 //    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 //    var date_string = date.format(formatter)
+    var rating_string = rating.toString()
 }
