77b5e271713b375b981a04e7da191d323f54e553
Suzi Ryu <ryusu@ryusu@sheridancollege.ca>
2021-04-18 01:33:16 -0400

    added recycle view layout

--- app/src/main/java/com/example/meetmypetbuddy/adapters/PetsAdapter.kt
+++ app/src/main/java/com/example/meetmypetbuddy/adapters/PetsAdapter.kt
@@ -1,2 +1,35 @@
 package com.example.meetmypetbuddy.adapters
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.meetmypetbuddy.databinding.PetRowLayoutBinding
+import com.example.meetmypetbuddy.models.Pet
+
+class PetsViewHodler(private val binding: PetRowLayoutBinding) :RecyclerView.ViewHolder(binding.root){
+    fun bind(p:Pet){
+        binding.bndPet = p
+        binding.executePendingBindings()
+    }
+}
+
+
+class PetsAdapter(val pets:MutableList<Pet>, val context: Context)
+    :RecyclerView.Adapter<PetsViewHodler>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PetsViewHodler {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = PetRowLayoutBinding.inflate(layoutInflater, parent, false)
+
+        return PetsViewHodler(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return pets.size
+    }
+
+    override fun onBindViewHolder(holder: PetsViewHodler, position: Int) {
+        val currPet = this.pets.get(position)
+        holder?.bind(currPet)
+    }
+}
